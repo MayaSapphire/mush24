9f460455bc57cffbecd95542be8f231e2aed5582
aNamee <brendenmckinley06@gmail.com>
2024-08-04 10:22:56 -0800

    Setup discord api

---
+++ main.py
@@ -0,0 +1,32 @@
+import discord
+import mush
+
+BOT_NAME = "Mush '24"
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Client(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    guild_count = 0
+
+    for guild in bot.guilds:
+        print(f"- {guild.id} (name: {guild.name})")
+        guild_count += 1
+
+    print(f"{BOT_NAME} is in {str(guild_count)} guilds.")
+
+
+@bot.event
+async def on_message(message):
+    to_send = mush.send_message(message)
+    await message.channel.send(to_send)
+
+def push_to_main(message:str):
+    for guild in bot.guilds:
+        guild.get_channel(1269444451684843563).send(message)
+
+bot.run("MTI2OTQzODcyODE2NjcwMzIwOQ.Gik2VD.dy_DqSsC0_v9ciHvKUFnj2iO31kEVmivKH0x2Y")
--- mush.py
+++ mush.py
@@ -1,26 +1,27 @@
-from time import sleep
-import random
+new_message = ""
 
-default_message_time = 1
+def send_message(message: str, slash_n: bool = True):
+    global new_message
+    new_message += message
+    if slash_n:
+        new_message += "\n"
 
-def message(message: str, time:float=default_message_time):
-    for char in message:
-        print(char, end='')
-        sleep(time/len(message))
-message("Welcome to Mush '24.\n")
-message("Your name is Kim Jin Su. You are the commander of the Daedalus, ")
-message("a ship launched as a liberation effort from the grasp of the Mush. \n")
-message("You were too late to save your home planet, but it's not too late for you and your crewmates.\n")
-message("You have two win conditions: you and your crew can discover the coordinates of a planet called Eden, ")
-message("or free the prisoners on a planet called Moros.\n")
-message("You need to achieve one of these win conditions before your ship is destroyed or your crew perishes.\n")
+
+send_message("Welcome to Mush '24.\n")
+send_message("Your name is Kim Jin Su. You are the commander of the Daedalus, ")
+send_message("a ship launched as a liberation effort from the grasp of the Mush. \n")
+send_message("You were too late to save your home planet, but it's not too late for you and your crewmates.\n")
+send_message("You have two win conditions: you and your crew can discover the coordinates of a planet called Eden, ")
+send_message("or free the prisoners on a planet called Moros.\n")
+send_message("You need to achieve one of these win conditions before your ship is destroyed or your crew perishes.\n")
+
 
 class Ship():
     def __init__(self, name, max_hull):
         self.name = name
         self.max_hull = max_hull
         self.hull = self.max_hull
-    
+
     def change_hull(self, change):
         if self.hull + change >= self.max_hull:
             self.hull = self.max_hull
@@ -30,9 +31,11 @@ class Ship():
             # ka-booooooooom!
             pass
 
+
 class Hunter(Ship):
     def __init__(self, name, max_hull):
         pass
+
 
 class PlayerShip(Ship):
     def __init__(self, name, max_hull, crew, oxygen, max_oxygen, fuel, max_fuel):
@@ -41,8 +44,8 @@ class PlayerShip(Ship):
         self.hull = self.max_hull
         self.crew = crew
         self.dead_crew = 0
-        self.ap = self.crew*8
-        self.max_ap = self.crew*12
+        self.ap = self.crew * 8
+        self.max_ap = self.crew * 12
         self.oxygen = oxygen
         self.max_oxygen = max_oxygen
         self.fuel = fuel
@@ -64,59 +67,69 @@ class PlayerShip(Ship):
         return alerts
 
     def change_hull(self, change):
-        if self.hull + change >= self.max_hull: # if the repair would make the hull go above max
-            self.hull = self.max_hull # just set it to the max
-        else: # otherwise
-            self.hull = self.hull + change # add the hull
+        if self.hull + change >= self.max_hull:  # if the repair would make the hull go above max
+            self.hull = self.max_hull  # just set it to the max
+        else:  # otherwise
+            self.hull = self.hull + change  # add the hull
         if self.hull <= 0:
             # ka-booooooooom!
-            message("NERON: Alert: Hull levels critical. Total destruction imminent. [kzz] ", time=2.5)
-            message("What have you done?! [Hax!] ", time=1.5)
-            message("I... I feel- [kzz] ", time=2)
-            message("Red alert: all humans p-ple-e-ease evacuate to the nearest esc... escape pods... wa-ait, wha-a-at do you mean we have no escape po-ods?! ", time=7)
-            message("[Hax!] I- I regret ev- everyth-th-iii-- [kzzzzzzz]\n", time=5)
-            message("The Daedalus explodes. Your mission ends in failure.\n", time=5)
-            message("Better luck next time!\n")
+            send_message("NERON: Alert: Hull levels critical. Total destruction imminent. [kzz] ", time=2.5)
+            send_message("What have you done?! [Hax!] ", time=1.5)
+            send_message("I... I feel- [kzz] ", time=2)
+            send_message(
+                "Red alert: all humans p-ple-e-ease evacuate to the nearest esc... escape pods... wa-ait, wha-a-at do you mean we have no escape po-ods?! ",
+                time=7)
+            send_message("[Hax!] I- I regret ev- everyth-th-iii-- [kzzzzzzz]\n", time=5)
+            send_message("The Daedalus explodes. Your mission ends in failure.\n", time=5)
+            send_message("Better luck next time!\n")
             input("Press enter to continue...")
             exit()
 
 
-
 daedalus = PlayerShip('Daedalus', max_hull=100, crew=16, oxygen=24, max_oxygen=32, fuel=16, max_fuel=32)
 
+
 def help_command():
-    print("Commands:")
-    print("help  : Shows list of commands.")
-    print("status: Displays ship status.")
+    send_message("Commands:")
+    send_message("help  : Shows list of commands.")
+    send_message("status: Displays ship status.")
+
 
 def status_command():
-    print("Ship status:")
-    print(f"  Hull: {daedalus.hull}/{daedalus.max_hull}")
-    print(f"Oxygen: {daedalus.oxygen}/{daedalus.max_oxygen}")
-    print(f"  Fuel: {daedalus.fuel}/{daedalus.max_fuel}")
+    send_message("Ship status:")
+    send_message(f"  Hull: {daedalus.hull}/{daedalus.max_hull}")
+    send_message(f"Oxygen: {daedalus.oxygen}/{daedalus.max_oxygen}")
+    send_message(f"  Fuel: {daedalus.fuel}/{daedalus.max_fuel}")
 
-    print("Crew status:")
-    print(f"Alive crewmates: {daedalus.crew}")
-    print(f" Dead crewmates: {daedalus.dead_crew}")
-    print(f"  Action points: {daedalus.ap}/{daedalus.max_ap}")
+    send_message("Crew status:")
+    send_message(f"Alive crewmates: {daedalus.crew}")
+    send_message(f" Dead crewmates: {daedalus.dead_crew}")
+    send_message(f"  Action points: {daedalus.ap}/{daedalus.max_ap}")
 
     if len(daedalus.alerts) == 0:
-        print("No alerts.")
+        send_message("No alerts.")
         return
-    print("Alerts:")
+    send_message("Alerts:")
     for alert in daedalus.alerts():
-        print(f"- {alert}")
+        send_message(f"- {alert}")
 
-playing = True
-while playing:
-    command = input("\nPlease enter a command: ").lower()
+
+
+# main.push_to_main(new_message)
+
+def send_message(message):
+    global new_message
+    new_message = ""
+    command = message.content.lower()
+    print(command)
     if command == 'help':
         help_command()
     elif command == 'status':
         status_command()
     elif command == 'a command':
-        print("You're very funny.")
-        print("A space pebble collides with the Daedalus.")
+        message("You're very funny.")
+        message("A space pebble collides with the Daedalus.")
         daedalus.change_hull(-10)
     else:
-        print("Invalid command. Type 'help' for help.")
+        message("Invalid command. Type 'help' for help.")
+    return new_message
